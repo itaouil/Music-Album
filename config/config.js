4f5d4e75b55572fa64304e36b9654eab32869802
Ilyass Taouil <itaouil95@gmail.com>
2017-05-25 10:59:13 +0200

    Finished full CRUD functionality.

--- config/config.js
+++ config/config.js
@@ -1,10 +1,1 @@
-var config = {
-  apiKey: "AIzaSyA8Ie7V4GYoUpMXNKYkzbya5LY4lZHy_xU",
-  authDomain: "music-album.firebaseapp.com",
-  databaseURL: "https://music-album.firebaseio.com",
-  projectId: "music-album",
-  storageBucket: "music-album.appspot.com",
-  messagingSenderId: "667521048350"
-};
-
-module.exports = config;
+// Here goes your firebase stuff
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,82 @@
 var express = require('express');
 var router = express.Router();
 
+// Firebase module
+var firebase = require('firebase');
+
 // Renders registration page
 router.get('/register', function(req, res, next) {
   res.render('users/register');
+});
+
+// Handles Registration
+router.post('/register', function(req, res, next) {
+
+  // Body attributes
+  var first_name = req.body.first_name;
+  var last_name  = req.body.last_name;
+  var email      = req.body.email;
+  var password   = req.body.password;
+  var password2  = req.body.password2;
+
+  // Validation
+  req.checkBody('first_name', 'First name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+  // Validation errors
+  var errors = req.validationErrors();
+
+  if (errors) {
+    // Render errors
+    res.render('users/register', {
+      errors: errors
+    });
+  }
+
+  else {
+    // firebase.createUser({
+    //   email    : email,
+    //   password : password
+    // }, function(err, userData){
+    //   if (err) {
+    //     console.log("Error creating user: ", err);
+    //   }
+    //   else {
+    //     console.log("Successfully created user with uid: ", userData.uid);
+    //     var user = {
+    //       uid: userData.uid,
+    //       email: email,
+    //       first_name: first_name,
+    //       last_name: last_name
+    //     }
+    //
+    //     var userRef = firebase.database().ref('users');
+    //     userRef.push(user);
+    //
+    //     req.flash('success_msg', 'You are now registered and can login');
+    //     res.redirect('/users/login');
+    //   }
+    // });
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+
+      // Handle Errors
+      if (error) {
+        res.render('users/register', {authError: error.message});
+      }
+
+      else {
+        // All good
+        console.log("Successfully created user");
+      }
+
+    });
+
+  }
+
 });
 
 // Renders login page
